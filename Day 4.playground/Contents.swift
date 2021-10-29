b543a21fe0202848de09ea3b84f6a57bd07ccb4f
Cober <cobre@cobre.dev>
2021-10-28 20:44:22 -0400

    day 4

---
+++ Day 4.playground/Contents.swift
@@ -0,0 +1,22 @@
+import Cocoa
+
+let surname = "Lasso"
+var score = 0
+
+enum UIStyle {
+    case light, dark, system
+}
+
+var style = UIStyle.dark
+
+let username: String
+username = "@cobredev"
+print(username)
+
+
+var strArray = ["Debut", "Fearless", "Speak Now", "Red", "1989", "reputation", "Lover", "folklore", "evermore", "evermore"]
+
+print(strArray.count)
+
+let strSet = Set(strArray)
+print(strSet.count)
